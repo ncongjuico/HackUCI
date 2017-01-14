548439b0f4ab833a564aa8771c49232a4dd4debb
shannonhu <shannonhu519@gmail.com>
2017-01-14 13:09:43 -0800

    Changed findBar with resultbar one the side

--- js/findBar.js
+++ js/findBar.js
@@ -1,43 +1,72 @@
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
       var map;
 
-      function init(){
-        //centered on UCI's LatLng
-        var center = new google.maps.LatLng(33.6404952,-117.8442962);
+      function initMap() {
+        var pyrmont = {lat: 34.0522, lng: -118.2437};
+
         map = new google.maps.Map(document.getElementById('map'), {
-          center: center,
+          center: pyrmont,
           zoom: 13
         });
 
-        var request = {
-          location: center,
-          radius: 8000,
-          types: ['bar']
-        };
-        infowindow = new google.maps.InfoWindow();
-
-        //search for bars
         var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
+        service.nearbySearch({
+          location: pyrmont,
+          radius: 5000,
+          type: ['bar']
+        }, processResults);
       }
 
-      //making sure we have results and no error connecting to google's servers
-      function callback(results, status){
-        if(status == google.maps.places.PlacesServiceStatus.OK){
-          //adds markers on map
-          for(var i = 0; i < results.length; i++){
-            createMarker(results[i]);
+      function processResults(results, status, pagination) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          return;
+        } else {
+          createMarkers(results);
+
+          if (pagination.hasNextPage) {
+            var moreButton = document.getElementById('more');
+
+            moreButton.disabled = false;
+
+            moreButton.addEventListener('click', function() {
+              moreButton.disabled = true;
+              pagination.nextPage();
+            });
           }
         }
       }
 
-      //places markers on map based on results array
-      function createMarker(place){
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
+      function createMarkers(places) {
+        var bounds = new google.maps.LatLngBounds();
+        var placesList = document.getElementById('places');
+
+        for (var i = 0, place; place = places[i]; i++) {
+          var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+          var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+          });
+
+
+          //showing as list on the sidebar  
+          placesList.innerHTML += '<li>' + place.name + '</li>';
+          bounds.extend(place.geometry.location);
+
+
+        }
+        map.fitBounds(bounds);
 
         //listener to display infowindow when you click on marker
         google.maps.event.addListener(marker, 'click', function(){
@@ -45,5 +74,3 @@
           infowindow.open(map,this);
         });
       }
-
-      google.maps.event.addDomListener(window, 'load', init);
