f672b8761da4a1f646bd27579e603ef0ea8871a4
Ney Congjuico <ncongjuico@yahoo.com>
2017-01-15 02:16:28 -0800

    reportYes update but not finished

--- js/reportYes.js
+++ js/reportYes.js
@@ -1,20 +1,31 @@
+// var map;
+//
+// //Initiate the Map
+// function initMap() {
+//   var pyrmont = {lat: 34.0522, lng: -118.2437};
+//
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: pyrmont,
+//     zoom: 13
+//   });
+//
+//   var service = new google.maps.places.PlacesService(map);
+//
+//   service.nearbySearch({
+//     location: pyrmont,
+//     radius: 5000,
+//     type: ['bar']
+//   }, processResults, callback);
+// }
 
-      var map;
+// smear out function
+var fadeOut = function fadeOut(section) {
+    section.classList.remove('fadeIn');
+    section.classList.add('fadeOut');
+};
 
-      //Initiate the Map
-      function initMap() {
-        var pyrmont = {lat: 34.0522, lng: -118.2437};
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 13
-        });
-
-        var service = new google.maps.places.PlacesService(map);
-
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 5000,
-          type: ['bar']
-        }, processResults, callback);
-      }
+// smear in function
+var fadeIn = function fadeIn(section) {
+    section.classList.remove('fadeOut');
+    section.classList.add('fadeIn');
+};
